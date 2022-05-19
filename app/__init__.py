ae79ba993e9b1460dc12022379ce8341e6a8d1ab
Mohamedlee27 <leonk8070@gmail.com>
2022-05-19 09:33:28 +0300

    created registration and login classes

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+# from .config import config
+app=Flask(__name__)
+
+
+
+from app import views
---
+++ app/forms.py
@@ -0,0 +1,24 @@
+import email
+from click import confirm
+from flask import Flask
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired,Email,EqualTo, ValidationError,Length
+from wtforms import StringField,SubmitField,TextAreaField,PasswordField,BooleanField
+# from app.models import User
+
+
+
+class LoginForm(FlaskForm):
+    email=StringField('Email', validators=[DataRequired(), Email()])
+    password=PasswordField('Password', validators=[DataRequired()])
+    login=SubmitField('Login')
+
+class Registration(FlaskForm):
+    username=StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email=StringField('Email', validators=[DataRequired()])
+    password=PasswordField('Password', validators=[DataRequired()])
+    confirm_password=PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
+    sign=SubmitField('Sign up')
+
+
+
