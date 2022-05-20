7c2797c440b60c2a5484ac4687ec3cbf784a9568
Mohamedlee27 <leonk8070@gmail.com>
2022-05-20 21:52:31 +0300

    deployment

--- app/config.py
+++ app/config.py
@@ -2,8 +2,7 @@ import os
 basedir=os.path.abspath(os.path.dirname(__file__))
 class Config(object):
      SECRET_KEY = os.environ.get('SECRET_KEY') or '7771'
-     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+     SQLALCHEMY_DATABASE_URI = 'postgresql://ahfxujedqnmymg:f8c2ddde4d20fe7a3a680ec0b69e2bcbfe4216a9ed470da9ccc55f1c9d563aaf@ec2-3-228-235-79.compute-1.amazonaws.com:5432/dqr8jm0hhg9ep'
      SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(Config):
