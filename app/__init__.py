9458b5a5c838847f997cadf547925dc4be9a1695
Mohamedlee27 <leonk8070@gmail.com>
2022-05-20 20:21:36 +0300

    made the comment work

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import  Migrate
 from flask_login import LoginManager
-from flask_bootstrap import bootstrap
+from flask_bootstrap import Bootstrap
 
-
+bootstrap = Bootstrap()
 app=Flask(__name__)
 app.config.from_object(Config)
 db=SQLAlchemy(app)
+bootstrap.init_app(app)
 
 migrate=Migrate(app, db)
 login=LoginManager(app)
